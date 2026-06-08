22aa71dc3553437fc4ac76b26d375ee3eadae747
jsibbickrbt <j.sibbick@rbtautomate.com>
2026-06-07 20:27:57 -0400

    fix: remove old functions with missing secrets from deploy

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,4 @@
-export { checkOverdue } from "./src/checkOverdue.js";
-export { weeklyDamageReport } from "./src/weeklyDamageReport.js";
-export { annualInspectionCheck } from "./src/annualInspectionCheck.js";
-export { updateOncallForwarding } from "./src/updateOncallForwarding.js";
+// SkySuite Console functions
 export { sendTestSms } from "./src/sendTestSms.js";
 export { sendPasswordResetEmail } from "./src/sendPasswordResetEmail.js";
 export { exportUserIcs } from "./src/exportUserIcs.js";
